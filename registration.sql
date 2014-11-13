8bb35a4a8063a797e62ed237e35d9cf36b51f260
Eden Mar <emar@ualberta.ca>
2014-11-13 16:37:04 -0700

    Registration 99% done. Need to redirect to correct places and commit after insert but looks good

--- registration.sql
+++ registration.sql
@@ -2,7 +2,13 @@
 * This is a test table for the registration module. We can rename or use a different one
 * for more extensive tests later --EM
 */
+
+/*first insert test:
+* try to insert values('yellow', 'pikachu', 'ash', 'ketchum', '0 pallet town', '1234567890');
+*/
 drop table registration;
 create table registration(username char(20), pass char(20), first char(20), last char(20), address varchar(30), phone int);
 insert into registration values('blue', 'squirtle', 'ash', 'ketchum', '0 pallet town', '1234567890');
 insert into registration values('red', 'charmander', 'ash', 'ketchum', '0 pallet town', '1234567890');
+
+
