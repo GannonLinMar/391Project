b6c97b7d6d751ed2da71ddeb3797a43d626b0f72
uaLogan <lglin@ualberta.ca>
2014-11-23 17:18:03 -0700

    demo setup folder

---
+++ SqlSetup/demo/index.sql
@@ -0,0 +1,4 @@
+/* Create the 3 indices required*/
+create index indexSubject on images(subject) indextype is ctxsys.context;
+create index indexPlace on images(place) indextype is ctxsys.context;
+create index indexDescription on images(description) indextype is ctxsys.context;
---
+++ SqlSetup/demo/popular.sql
@@ -0,0 +1,3 @@
+drop table popularity;
+
+create table popularity(photo_id int, username varchar(24)) tablespace c391ware;
